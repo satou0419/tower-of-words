eaa5240e6fafa1ce330feee09f2b51488cff10d8
Acrylus <theantonjoseph@gmail.com>
2023-11-18 11:28:53 +0800

    TowerName Added

---
+++ src/TowerName.js
@@ -0,0 +1,21 @@
+import "./TowerName.css";
+import "./root.css";
+
+function TowerName() {
+  return <>
+        <div className="main-towername">
+            <div className="towername-top-container">
+                <button className="btn-back btn-back-towername">BACK</button>
+                <h1 className="header-towername">
+                Tower Name
+                </h1>
+                <button className="btn-next btn-next-towername">BACK</button>
+            </div>
+            <div className="main-container">
+
+            </div>
+        </div>
+  </>
+}
+
+export default TowerName;
