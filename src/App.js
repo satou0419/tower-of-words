bd263f1618b82e9d02983b6380bbdfbadd689bbf
Reyd <officialgarciareydante@gmail.com>
2023-11-18 08:30:24 +0800

    Routing Performed.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
-import "./App.css";
+// App.js
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navigation from "./Navigation";
+import Archive from "./Archive";
+import Items from "./Items";
+import Dashboard from "./Dashboard";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigation />}>
+          <Route path="/archive" element={<Archive />} />
+          <Route path="/items" element={<Items />} />
+          <Route path="/home" element={<Dashboard />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
