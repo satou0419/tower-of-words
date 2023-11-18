b4aebf4a86ff4844e2ae83f4f81ef5a18119b68b
Steemp <gilyabao@gmail.com>
2023-11-18 12:27:01 +0800

    Added adventure activity and some image assets

---
+++ src/AdventureActivity.js
@@ -0,0 +1,41 @@
+import './TheGame.css';
+
+export default function AdActivity(){
+
+    return<div className='mother-of-all'>
+            <div className="screen">
+
+            </div>
+
+            <div className="ui">
+                {/* <img src="./frame.png"/> */}
+                <div className="common-nga-class left">
+                    <h2>Items</h2>
+
+                    <div className="items-container">
+                        <div className="container-upper">
+                            <button className="item-button"> <img src='./images/small_bandage.png'/></button>
+                            <button className="item-button"> <img src='./images/health_kit.png'/></button>
+                            <button className="item-button"> <img src='./images/unusual_battery.png'/></button>
+                        </div>
+                        
+                        <div className="container-lower">
+                            <button className="item-button"> 1</button>
+                            <button className="item-button"> item 5</button>
+                            <button className="item-button"> item 6</button>
+                        </div>
+                    </div>
+                </div>
+
+                <div className='common-nga-class center'>
+                    asda
+                </div>
+
+                <div className='common-nga-class right'>    
+                    asdadw
+                </div>
+                
+            </div>
+        </div>
+    
+}
