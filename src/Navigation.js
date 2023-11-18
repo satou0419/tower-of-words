c4f774a95322c450404e30ddeaa457044675de55
Reyd <officialgarciareydante@gmail.com>
2023-11-18 08:33:13 +0800

    Link & Outlet Performed

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,6 +1,8 @@
+import { Link, Outlet } from "react-router-dom";
 import "./Navigation.css";
 import "./root.css";
 import "@fontsource/lilita-one";
+
 export default function Navigation() {
   return (
     <div className="wrapper">
@@ -11,12 +13,26 @@ export default function Navigation() {
           <span className="currency">1000</span>
         </section>
         <section className="nav-links">
-          <li className="push-right">HOME</li>
-          <li>ITEMS</li>
-          <li>ARCHIVE</li>
-          <li>LOGOUT</li>
+          <li className="push-right">
+            <Link to="/home">HOME</Link>
+          </li>
+
+          <li>
+            <Link to="/items">ITEMS</Link>
+          </li>
+
+          <li>
+            <Link to="/archive">ARCHIVE</Link>
+          </li>
+          <li>
+            <Link>LOGOUT</Link>
+          </li>
         </section>
       </nav>
+
+      <section className="content">
+        <Outlet />
+      </section>
     </div>
   );
 }
