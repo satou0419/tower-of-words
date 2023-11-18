eaf35bcfd47b752ab45d4f1a095b9ecab6424011
Reyd <officialgarciareydante@gmail.com>
2023-11-19 03:04:57 +0800

    updated info

--- src/App.js
+++ src/App.js
@@ -5,13 +5,18 @@ import Navigation from "./Navigation";
 import Archive from "./Archive";
 import Items from "./Items";
 import Dashboard from "./Dashboard";
+import Logout from "./Logout";
+import AdActivity from "./AdventureActivity";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Navigation />}>
-          <Route path="/archive" element={<Archive />} />
+          <Route path="/archive" element={<AdActivity />} />
+
+          <Route path="/logout" element={<Logout />} />
+
           <Route path="/items" element={<Items />} />
           <Route path="/home" element={<Dashboard />} />
         </Route>
--- src/Logout.js
+++ src/Logout.js
@@ -2,19 +2,19 @@ import "./Logout.css";
 import "./root.css";
 import "@fontsource/lilita-one";
 
-export default function Logout(){
-    return(
-        <>
-        <div className="logout">
+export default function Logout() {
+  return (
+    <>
+      <div className="logout">
         <div className="logout-pane">
-        <h1>Logout Account?</h1>
-        <img src="./images/sad robot.png" className="logout-icon"></img>
-        <div className="btns">
+          <h1>Logout Account?</h1>
+          <img src="./images/sad robot.png" className="logout-icon"></img>
+          <div className="btns">
             <button className="btn-cancel cancel">Cancel</button>
             <button className="btn-logout log ">Logout</button>
+          </div>
         </div>
-        </div>
-        </div>
-        </>
-    )
+      </div>
+    </>
+  );
 }
