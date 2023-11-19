2666d4bc073b105c2a0d5d11bd730b491a5de7b4
Reyd <officialgarciareydante@gmail.com>
2023-11-20 05:32:56 +0800

    UPDATE: Logout to Modal

--- src/Logout.js
+++ src/Logout.js
@@ -1,20 +1,29 @@
+import React from "react";
 import "./Logout.css";
 import "./root.css";
 import "@fontsource/lilita-one";
 
-export default function Logout() {
+export default function Logout({ onClose }) {
   return (
-    <>
-      <div className="logout">
-        <div className="logout-pane">
+    <div className="logout-overlay">
+      <div className="logout-pane">
+        <div className="details">
           <h1>Logout Account?</h1>
-          <img src="./images/sad robot.png" className="logout-icon"></img>
-          <div className="btns">
-            <button className="btn-cancel cancel">Cancel</button>
-            <button className="btn-logout log ">Logout</button>
-          </div>
+
+          <img
+            src="./images/sad robot.png"
+            className="logout-icon"
+            alt="Sad Robot"
+          />
+        </div>
+
+        <div className="btns">
+          <button className="btn-cancel" onClick={onClose}>
+            Cancel
+          </button>
+          <button className="btn-logout">Logout</button>
         </div>
       </div>
-    </>
+    </div>
   );
 }
