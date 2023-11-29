52e6a6e0db8d2bb7996004af1ac98e1bac0a574d
Acrylus <theantonjoseph@gmail.com>
2023-11-30 02:09:05 +0800

    Update Custom Tower Error

--- src/ViewCustomTower.js
+++ src/ViewCustomTower.js
@@ -29,7 +29,7 @@ function ViewCustomTower() {
             const towerNames = data.map(tower => tower.towername);
             setFilteredCustomTower(towerNames);
 
-            const user = data.flatMap(tower => tower.participantslist.map(participant => participant.username));
+            const user = data.map(tower => tower.participantslist.map(participant => participant.username));
             setFilteredParticipants(user);
             console.log(user);
         })
@@ -38,122 +38,110 @@ function ViewCustomTower() {
         });
     },[]);
 
-  const towerClicked = (index) => {
-    const newClickedTower = index === clickedTower ? null : index;
+    const towerClicked = (index) => {
+        const newClickedTower = index === clickedTower ? null : index;
 
-    setClickedTower(newClickedTower);
-    setIsRightContainerVisible(newClickedTower !== null);
-  };
+        setClickedTower(newClickedTower);
+        setIsRightContainerVisible(newClickedTower !== null);
 
-  const customTowerSearchChange = (event) => {
-    const searchTerm = event.target.value;
-    setSearchCustomTower(searchTerm);
-    setClickedTower(null);
-    const filtered = customTower.filter((word) =>
-      word.toLowerCase().includes(searchTerm.toLowerCase())
-    );
-    setFilteredCustomTower(filtered);
-  };
+        if (newClickedTower !== null) {
+            const participants = data[newClickedTower].participantslist.map(participant => participant.username);
+            setFilteredParticipants(participants);
+        }
+    };    
 
-  const participantsSearchChange = (event) => {
-    const searchTerm = event.target.value;
-    setSearchParticipant(searchTerm);
-    const filtered = participants.filter((word) =>
-      word.toLowerCase().includes(searchTerm.toLowerCase())
-    );
-    setFilteredParticipants(filtered);
-  };
+    const customTowerSearchChange = (event) => {
+        const searchTerm = event.target.value;
+        setSearchCustomTower(searchTerm);
+        setClickedTower(null);
+        const filtered = data.filter(tower => tower.towername.toLowerCase().includes(searchTerm.toLowerCase()));
+        setFilteredCustomTower(filtered.map(tower => tower.towername));
+    };
+    
+    const participantsSearchChange = (event) => {
+        const searchTerm = event.target.value;
+        setSearchParticipant(searchTerm);
+        const filtered = data.filter(tower => tower.participantslist.map(participant => participant.username)).filter(username => username.toLowerCase().includes(searchTerm.toLowerCase()));
+        setFilteredParticipants(filtered);
+    };
 
-  return (
-    <>
-      <div className="main-viewcustomtower">
-        <div className="viewcustomtower-top-container">
-          <button className="btn-back btn-back-viewcustomtower">BACK</button>
+    return <>
+            <div className="main-viewcustomtower">
+                <div className="viewcustomtower-top-container">
+                    <button className="btn-back btn-back-viewcustomtower">BACK</button>
 
-          <button className="btn-next btn-next-viewcustomtower">GAME</button>
-        </div>
-        <div className="main-container-overall">
-          <div className="main-container-left">
-            <div className="main-border-view">
-              <div className="main-top-viewcustomtower">
-                <img
-                  src="./images/search.png"
-                  height={30}
-                  alt="search"
-                  className="search-icon"
-                />
-                <input
-                  className="inputs search-viewcustomtower"
-                  type="text"
-                  value={searchCustomTower}
-                  onChange={customTowerSearchChange}
-                  placeholder="Search Tower Name"
-                />
-              </div>
-
-              <div className="word-container-scroll-viewcustomtower">
-                {filteredCustomTower.map((name, index) => (
-                  <div
-                    key={index}
-                    className={`word-container-viewcustomtower ${
-                      index === clickedTower ? "clicked" : ""
-                    }`}
-                    onClick={() => towerClicked(index)}
-                  >
-                    <h4 className="words-viewcustomtower">{name}</h4>
-                  </div>
-                ))}
-              </div>
-            </div>
-          </div>
-
-          <div className="divider"></div>
-
-          <div className="main-container-right">
-            <div className="main-border-view">
-              {isRightContainerVisible ? (
-                <div className="main-container-right-inner">
-                  <div className="main-top-viewparticipants">
-                    <img
-                      src="./images/search.png"
-                      height={30}
-                      alt="search"
-                      className="search-icon"
-                    />
-                    <input
-                      className="inputs search-viewparticipants"
-                      type="text"
-                      value={searchParticipant}
-                      onChange={participantsSearchChange}
-                      placeholder="Search Participant"
-                    />
-                    <button className="btn-next btn-next-viewparticipants">
-                      FILTER
-                    </button>
-                  </div>
-
-                  <div className="word-container-scroll-viewparticipants">
-                    {filteredParticipants.map((firstname, index) => (
-                      <div
-                        key={index}
-                        className="word-container-viewcustomtower"
-                      >
-                        <h4 className="words-viewcustomtower">{firstname}</h4>
-                      </div>
-                    ))}
-                  </div>
-
-                  <div className="code-viewparticipants">CODE: TEST</div>
+                    <button className="btn-next btn-next-viewcustomtower">GAME</button>
                 </div>
-              ) : (
-                <div className="main-container-right-blank"></div>
-              )}
+                <div className="main-container-overall">
+                    <div className="main-container-left">
+                        <div className="main-border-view">
+                            <div className="main-top-viewcustomtower">
+                                <img src="./images/search.png" height={30} alt="search"  className="search-icon"/>
+                                <input 
+                                    className="inputs search-viewcustomtower"
+                                    type="text"
+                                    value={searchCustomTower}
+                                    onChange={customTowerSearchChange}
+                                    placeholder="Search Tower Name"
+                                /> 
+                            </div>
+
+                            <div className="word-container-scroll-viewcustomtower">
+                                {filteredCustomTower.map((name, index) => (
+                                    <div key={index} className={`word-container-viewcustomtower ${index === clickedTower ? 'clicked' : ''}`} onClick={() => towerClicked(index)}>
+                                        <h4 className="words-viewcustomtower">
+                                            {name}
+                                        </h4>
+                                    </div>
+                                ))}
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="divider">
+
+                    </div>
+
+                    <div className="main-container-right">
+                        <div className="main-border-view">
+                            {isRightContainerVisible ? (
+                                <div className="main-container-right-inner">
+                                    <div className="main-top-viewparticipants">
+                                        <img src="./images/search.png" height={30} alt="search"  className="search-icon"/>
+                                        <input 
+                                            className="inputs search-viewparticipants" 
+                                            type="text"
+                                            value={searchParticipant}
+                                            onChange={participantsSearchChange}
+                                            placeholder="Search Participant"
+                                        /> 
+                                        <button className="btn-next btn-next-viewparticipants">FILTER</button>
+                                    </div>
+
+                                    <div className="word-container-scroll-viewparticipants">
+                                        {filteredParticipants.map((username, index) => (
+                                            <div key={index} className="word-container-viewcustomtower">
+                                                <h4 className="words-viewcustomtower">
+                                                    {username}
+                                                </h4>
+                                            </div>
+                                        ))}
+                                    </div>
+                                    
+                                    <div className="code-viewparticipants">
+                                        CODE: TEST
+                                    </div>
+                                </div>
+                            ) : (
+                                <div className="main-container-right-blank">
+
+                                </div>
+                            )}
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
-        </div>
-      </div>
     </>
-  );
 }
 
 export default ViewCustomTower;
