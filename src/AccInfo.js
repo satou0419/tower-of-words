be5f95742e476f6c70f0e1f4eaaf0851f16787bb
insanity31 <riveratrishamaee@gmail.com>
2023-11-30 17:58:51 +0800

    updateeeeeee

--- src/AccInfo.js
+++ src/AccInfo.js
@@ -9,57 +9,17 @@ function AccInfo() {
         <div className="acc-info">
           <h2>ACCOUNT INFORMATION</h2>
           <input
-            className="inputs-information inputs-acc"
+            className="inputs-information-acc inputs-acc"
             type="text"
             placeholder="Username"
           ></input>
           <input
-            className="inputs-information inputs-acc"
+            className="inputs-information-acc inputs-acc"
             type="text"
             placeholder="Firstname"
           ></input>
           <input
-            className="inputs-information inputs-acc"
-            type="text"
-            placeholder="Lastname"
-          ></input>
-          <button className="btn-save save-acc">Save Changes</button>
-        </div>
-        <div className="pass">
-          <h2>Change Password</h2>
-          <input
-            className="inputs-information inputs-pass"
-            type="password"
-            placeholder="Old Password"
-          ></input>
-          <input
-            className="inputs-information inputs-pass"
-            type="password"
-            placeholder="New Password"
-          ></input>
-          <input
-            className="inputs-information inputs-pass"
-            type="password"
-            placeholder="Confirm New Password"
-          ></input>
-          <button className="btn-save save-pass">Save Changes</button>
-        </div>
-      </div>
-      <div className="account">
-        <div className="acc-info">
-          <h2>ACCOUNT INFORMATION</h2>
-          <input
-            className="inputs-information inputs-acc"
-            type="text"
-            placeholder="Username"
-          ></input>
-          <input
-            className="inputs-information inputs-acc"
-            type="text"
-            placeholder="Firstname"
-          ></input>
-          <input
-            className="inputs-information inputs-acc"
+            className="inputs-information-acc inputs-acc"
             type="text"
             placeholder="Lastname"
           ></input>
@@ -68,17 +28,17 @@ function AccInfo() {
         <div className="pass">
           <h2>CHANGE PASSWORD</h2>
           <input
-            className="inputs-information inputs-pass"
+            className="inputs-information-acc inputs-pass"
             type="password"
             placeholder="Current Password"
           ></input>
           <input
-            className="inputs-information inputs-pass"
+            className="inputs-information-acc inputs-pass"
             type="password"
             placeholder="New Password"
           ></input>
           <input
-            className="inputs-information inputs-pass"
+            className="inputs-information-acc inputs-pass"
             type="password"
             placeholder="Confirm New Password"
           ></input>
--- src/Shop.js
+++ src/Shop.js
@@ -5,42 +5,10 @@ import "@fontsource/lilita-one";
 import React, { useState } from "react";
 
 function Shop() {
-  const itemsData = ["1", "2", "3"];
-  const totalItems = itemsData.length;
-  const [currentIndex, setCurrentIndex] = useState(1);
-
-  const handlePrevClick = () => {
-    setCurrentIndex((prevIndex) => (prevIndex - 1 + totalItems) % totalItems);
-  };
-
   return (
     <>
       <div className="main-shop">
-        <div className="shop-top-container">
-          <button className="btn-back btn-back-shop">
-            <Link to="/inventory">BACK</Link>
-          </button>
-          <h1 className="header-shop">SHOP </h1>
-          <button className="btn-next btn-next-shop">NEXT</button>
-        </div>
-        <div className="main-container">
-          <div id="prev" onClick={handlePrevClick}>
-            &lt;
-          </div>
-          <div className="carousel">
-            {itemsData.map((item, index) => (
-              <div
-                key={index}
-                className={`item ${index === currentIndex ? "active" : ""}`}
-              >
-                {item}
-              </div>
-            ))}
-          </div>
-          <div id="next" onClick={handleNextClick}>
-            &gt;
-          </div>
-        </div>
+       
       </div>
     </>
   );
