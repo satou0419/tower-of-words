cd922b25a322bb8e750f0bf66463362fcb81c03d
Acrylus <theantonjoseph@gmail.com>
2023-12-16 23:56:26 +0800

    EnterCode

--- src/EnterCode.js
+++ src/EnterCode.js
@@ -1,44 +1,69 @@
-import { Link, useNavigate  } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import "./EnterCode.css";
 import "./root.css";
 import "@fontsource/lilita-one";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
+import { Context } from "./App";
 
 function EnterCode() {
+  const [userInfo] = useContext(Context);
   const [gameCode, setGameCode] = useState("");
   const navigate = useNavigate();
   const [data, setData] = useState([]);
-  
 
   useEffect(() => {
-    fetch('http://localhost:8080/CustomTower/getAllCustomTower')
-    .then(response => {
+    fetch(`http://localhost:8080/CustomTower/getByGameCode/${gameCode}`)
+      .then(response => {
         if (!response.ok) {
-        throw new Error('Network response was not ok');
+          throw new Error('Network response was not ok');
         }
         return response.json();
-    })
-    .then(data => {
-      setData(data);
-      console.log(data)
-    })
-    .catch(error => {
+      })
+      .then(data => {
+        setData(data);
+        console.log(data);
+      })
+      .catch(error => {
         console.error('There was a problem with the fetch operation:', error);
-    });
-  },[]);
-  
-  const handleCodeChange = (event) => {
+      });
+  }, [gameCode]);
+
+  const handleCodeChange = event => {
     setGameCode(event.target.value);
   };
 
   const handleJoinNow = () => {
-    if (gameCode.trim() === '') {
-      console.error('Game code cannot be empty');
+    if (gameCode.trim() === "") {
+      console.error("Game code cannot be empty");
       return;
     }
 
     console.log("Game Code:", gameCode);
-    navigate(`/custom-game/${gameCode}`);
+
+    if (loggedInUser) {
+      fetch(`http://localhost:8080/CustomTower/addParticipant/${gameCode}`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(loggedInUser),
+      })
+        .then(response => {
+          if (!response.ok) {
+            throw new Error("Network response was not ok");
+          }
+          return response.json();
+        })
+        .then(data => {
+          console.log("Participant added successfully:", data);
+          navigate(`/custom-game/${gameCode}`);
+        })
+        .catch(error => {
+          console.error("There was a problem adding the participant:", error);
+        });
+    } else {
+      console.error("User not logged in");
+    }
   };
 
   const goBack = () => {
